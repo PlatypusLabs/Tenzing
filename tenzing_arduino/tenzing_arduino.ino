d69adc2917597d9cf9ccd3bc57539f3700f1ab18
AshisGhosh <ashisghosh@live.com>
2018-04-22 13:39:03 -0700

    analog read started

--- tenzing_arduino/tenzing_arduino.ino
+++ tenzing_arduino/tenzing_arduino.ino
@@ -6,6 +6,23 @@ AF_DCMotor motor_R(1);
 AF_DCMotor motor_L(2);
 AF_DCMotor motor_S(3);
 
+#define sensorPin 0
+
+#define autoButton 1
+#define upButton  2
+#define downButton 3
+#define forwardButton 4
+#define backButton 5
+
+int sensorVal = 0;
+
+int autoVal = 0;
+int upVal = 0;
+int downVal = 0;
+int forwardVal = 0;
+int backVal = 0;
+
+bool autoFlag = false;
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
@@ -29,38 +46,46 @@ int i;
 
 // Test the DC motor, stepper and servo ALL AT ONCE!
 void loop() {
+
+  sensorVal = analogRead(sensorPin);
+  Serial.print("Sensor value is: ");     
+  Serial.println(sensorVal);
+
+  autoVal = analogRead(autoPin);
+  Serial.print("Auto value is: ");     
+  Serial.println(autoVal);
+
+  upVal = analogRead(upButton);
+  Serial.print("Up value is: ");     
+  Serial.println(upVal);
+
+  downVal = analogRead(downButton);
+  Serial.print("Down value is: ");     
+  Serial.println(downVal);
+
+  forwardVal = analogRead(forwardButton);
+  Serial.print("Forward value is: ");     
+  Serial.println(forwardVal);
+
+  backVal = analogRead(backButton);
+  Serial.print("Back value is: ");     
+  Serial.println(backVal);
+
+  if (autoFlag){
+    
+  }
+  
   motor_R.run(FORWARD);
   motor_L.run(FORWARD);
   motor_S.run(FORWARD);
   motor_S.setSpeed(255);
-  for (i=0; i<255; i++) {
-    motor_R.setSpeed(i);
-    motor_L.setSpeed(i);
-//    motor_S.setSpeed(i);  
-    delay(3);
- }
+//  for (i=0; i<255; i++) {
+//    motor_R.setSpeed(i);
+//    motor_L.setSpeed(i);
+////    motor_S.setSpeed(i);  
+//    delay(3);
+// }
  
-  for (i=255; i!=0; i--) {
-    motor_R.setSpeed(i);
-    motor_L.setSpeed(i);
-//    motor_S.setSpeed(i);  
-    delay(3);
- }
  
-  motor_R.run(BACKWARD);
-  motor_L.run(BACKWARD);
-//  motor_S.run(BACKWARD);
-  for (i=0; i<255; i++) {
-    motor_R.setSpeed(i);
-    motor_L.setSpeed(i);
-//    motor_S.setSpeed(i);  
-    delay(3);
- }
- 
-  for (i=255; i!=0; i--) {
-    motor_R.setSpeed(i); 
-    motor_L.setSpeed(i);
-//    motor_S.setSpeed(i); 
-    delay(3);
  }
 }
